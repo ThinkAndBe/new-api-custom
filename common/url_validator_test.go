07eb4036f616638eed2b5f92163e07923f476f87
bugking2493 <crysad@qq.com>
2026-08-20 15:49:33 +0800

    test: 修复上游遗留的 url_validator_test 编译错误（contains 未定义，改用 strings.Contains）

--- common/url_validator_test.go
+++ common/url_validator_test.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/QuantumNous/new-api/constant"
@@ -109,7 +110,7 @@ func TestValidateRedirectURL(t *testing.T) {
 					t.Errorf("ValidateRedirectURL(%q) expected error containing %q, got nil", tt.url, tt.errContains)
 					return
 				}
-				if tt.errContains != "" && !contains(err.Error(), tt.errContains) {
+				if tt.errContains != "" && !strings.Contains(err.Error(), tt.errContains) {
 					t.Errorf("ValidateRedirectURL(%q) error = %q, want error containing %q", tt.url, err.Error(), tt.errContains)
 				}
 			} else {
